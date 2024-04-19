537621d43ee2fc7981aac7881cc58356f6bf4085
unknown <ayushtomar306@gmail.com>
2024-04-19 16:22:04 +0530

    service section done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { PlansComponent } from './components/plans/plans.component';
 import { ConnectOnWhatsappComponent } from './components/connect-on-whatsapp/connect-on-whatsapp.component';
+import { PricingComponent } from './components/pricing/pricing.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     PlansComponent,
-    ConnectOnWhatsappComponent
+    ConnectOnWhatsappComponent,
+    PricingComponent,
+    ServicesComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
