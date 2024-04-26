1eec3bb6642bdeeb3548ea1fc7011eab059477cb
unknown <ayushtomar306@gmail.com>
2024-04-26 14:29:57 +0530

    add forms section

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,25 @@ export const routes: Routes = [
     {
         path:'',
         loadComponent:() => import('./components/home/home.component').then(m=>m.HomeComponent)
+    },
+    {
+        path:'plans',
+        loadComponent:()=> import('./components/home/components/plans/plans.component').then(m=>m.PlansComponent)
+    },
+    {
+        path:'speedTest',
+        loadComponent:()=>import('./components/speed-test/speed-test.component').then(m=>m.SpeedTestComponent)
+    },
+    {
+        path:'blog',
+        loadComponent:()=>import('./components/blog/blog.component').then(m=>m.BlogComponent)
+    },
+    {
+        path:'contactUs',
+        loadComponent:()=>import('./components/contact-us/contact-us.component').then(m=>m.ContactUsComponent)
+    },
+    {
+        path:'refer-a-friend',
+        loadComponent:()=>import('./components/refer-a-friend/refer-a-friend.component').then(m=>m.ReferAFriendComponent)
     }
 ];
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
